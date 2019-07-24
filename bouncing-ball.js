3a2aafbfc7ca18b86fed20210ae08ddf03860ee6
Kirill Zhurin <kirillzhurin@gmail.com>
2019-07-24 13:46:50 +0300

    moved ball to a separate file

--- bouncing-ball.js
+++ bouncing-ball.js
@@ -3,23 +3,6 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var g = 0.1; // acceleration due to gravity
 
-function Ball(radius, color) {
-  this.radius = radius;
-  this.color = color;
-  this.x = 0; // initial horizontal position
-  this.y = 0; // initial vertical position
-  this.vx = 0; // initial horizontal speed
-  this.vy = 0; // initial vertical speed
-}
-
-Ball.prototype.draw = function(context) {
-  context.fillStyle = this.color;
-  context.beginPath();
-  context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true);
-  context.closePath();
-  context.fill();
-}
-
 var ball = new Ball(50, '#8bc34a');
 ball.x = 50;
 ball.y = 50;
